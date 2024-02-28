9340240284bda2c3dd8448c4d4776656a4a86a5f
ygursu <yasemingrs@gmail.com>
2024-02-28 22:03:27 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp99/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilenikisayinincarpimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, carpim;
+
+            Console.WriteLine("Lütfen Birinci sayıyı giriniz");
+            sayi1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen İkinci sayıyı giriniz");
+            sayi2 = Int32.Parse(Console.ReadLine());
+
+            carpim = sayi1 * sayi2;
+
+            Console.WriteLine("Sayıların çarpımı =" + carpim);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
